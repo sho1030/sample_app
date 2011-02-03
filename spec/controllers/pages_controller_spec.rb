6ebb6721d1cf88bad884fabd8d3724518c8f8504
Hanol Seo <hanol.seo@headflow.net>
2011-02-04 00:53:26 +0900

    Initial commit

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,11 @@ describe PagesController do
 render_views
 
   describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
     it "should have the right title" do
       get 'home'
       response.should have_selector("title", 
@@ -12,18 +17,27 @@ render_views
   end
 
   describe "GET 'contact'" do
+     it "should be successful" do
+        get 'contact'
+        response.should be_success
+      end
+      
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("contact", 
-      :content => "Ruby on Rails Tutorial Sample App | contact")
+      response.should have_selector("title", 
+      :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
   
   describe "GET 'about'" do
+     it "should be successful" do
+        get 'about'
+        response.should be_success
+      end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("about",
-      :content => "Ruby on Rails Tutorial Sample App | about")
+      response.should have_selector("title",
+      :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
