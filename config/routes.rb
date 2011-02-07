397dfc116503cf4ac54f18217cc5838ab024df8d
Hanol Seo <hanol.seo@headflow.net>
2011-02-07 17:13:39 +0900

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  get "users/new"
+  match '/signup', :to => 'users#new'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to=> 'pages#about'
+  match '/help', :to=> 'pages#help'
   
-  get "pages/about"
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+            
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
